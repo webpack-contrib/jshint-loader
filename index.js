d514ebe64e88a65d508385634cc7fe0224617a32
Alex <trunks123@gmx.de>
2014-07-04 08:49:04 +0200

    add support for .jshintrc files

--- index.js
+++ index.js
@@ -3,11 +3,59 @@
 	Author Tobias Koppers @sokra
 */
 var jshint = require("jshint").JSHINT;
+var RcLoader = require("rcloader");
+var stripJsonComments = require("strip-json-comments");
 var loaderUtils = require("loader-utils");
-module.exports = function(input) {
-	this.cacheable && this.cacheable();
-	var options = {};
+var fs = require("fs");
 
+
+// setup RcLoader
+var rcLoader = new RcLoader(".jshintrc", null, {
+	loader: function(path) {
+		return path;
+	}
+});
+
+function loadRcConfig(callback){
+	var sync = typeof callback !== "function";
+
+	if(sync){
+		var path = rcLoader.for(this.context);
+		if(typeof path !== "string") {
+			// no .jshintrc found
+			return {};
+		} else {
+			this.addDependency(path);
+			var file = fs.readFileSync(path, "utf8");
+			return JSON.parse(stripJsonComments(file));
+		}
+	}
+	else {
+		rcLoader.for(this.context, function(err, path) {
+			if(typeof path !== "string") {
+				// no .jshintrc found
+				return callback(null, {});
+			}
+
+			this.addDependency(path);
+			fs.readFile(path, "utf8", function(err, file) {
+				var options;
+
+				if(!err) {
+					try {
+						options = JSON.parse(stripJsonComments(file));
+					}
+					catch(e) {
+						err = e;
+					}
+				}
+				callback(err, options);
+			});
+		}.bind(this));
+	}
+}
+
+function jsHint(input, options) {
 	// copy options to own object
 	if(this.options.jshint) {
 		for(var name in this.options.jshint) {
@@ -77,5 +125,30 @@ module.exports = function(input) {
 	}
 	if(failOnHint && !result)
 		throw new Error("Module failed in cause of jshint error.");
-	return input;
+}
+
+module.exports = function(input) {
+	this.cacheable && this.cacheable();
+	var callback = this.async();
+
+	if(!callback) {
+		// load .jshintrc synchronously
+		var config = loadRcConfig.call(this);
+		jsHint.call(this, input, config);
+		return input;
+	}
+
+	// load .jshintrc asynchronously
+	loadRcConfig.call(this, function(err, config) {
+		if(err) return callback(err);
+
+		try {
+			jsHint.call(this, input, config);
+		}
+		catch(e) {
+			return callback(e);
+		}
+		callback(null, input);
+
+	}.bind(this));
 }
